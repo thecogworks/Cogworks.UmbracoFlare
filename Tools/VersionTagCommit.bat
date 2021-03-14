8ddb8edab6439f4ab010c9998ad56b24a745fd29
Alejandro Ocampo <alejandro.ocampo@wearecogworks.com>
2021-03-13 19:12:41 -0500

    chore updated to 1.2.0

--- Tools/VersionTagCommit.bat
+++ Tools/VersionTagCommit.bat
@@ -12,11 +12,11 @@ set tag_without_spaces=%tag_without_spaces: =_%
 @ECHO off
 
 ECHO.
-..\Tools\AssemblyInfoUtil\AssemblyInfoUtil.exe -set:%VERSION%.* "..\Source\Cogworks.UmbracoFlare.Core\Properties\AssemblyInfo.cs"
-..\Tools\AssemblyInfoUtil\AssemblyInfoUtil.exe -set:%version%.* "..\Source\Cogworks.UmbracoFlare.UI\Properties\AssemblyInfo.cs"
+..\Tools\AssemblyInfoUtil\AssemblyInfoUtil.exe -set:%VERSION% "..\Source\Cogworks.UmbracoFlare.Core\Properties\AssemblyInfo.cs"
+..\Tools\AssemblyInfoUtil\AssemblyInfoUtil.exe -set:%VERSION% "..\Source\Cogworks.UmbracoFlare.UI\Properties\AssemblyInfo.cs"
 
 ECHO.
-git commit -am "Updated to version %version%"
+git commit -am "Updated to version %VERSION%"
 git tag "%tag_without_spaces%"
 ECHO.
 ECHO.
